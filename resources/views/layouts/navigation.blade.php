e6a0ab5b7a73e5debe20c6e4d90f7c25ea19870f
EylinJois <s160423031@student.ubaya.ac.id>
2026-03-26 09:00:07 +0700

    edit nav-link inn navigation view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="url()->previous()">
+                    <x-responsive-nav-link :href="url()->previous()">
                         {{ __('Kembali') }}
-                    </x-nav-link>
+                    </x-responsive-nav-link>
                 </div>
             </div>
 
